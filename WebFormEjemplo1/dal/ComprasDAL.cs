8decc447f816cbf8f134485bb3e4ae1cc6b8a5b4
nixonmonge <nixon.monje@gmail.com>
2019-06-26 21:48:40 -0400

    ok

--- WebFormEjemplo1/dal/ComprasDAL.cs
+++ WebFormEjemplo1/dal/ComprasDAL.cs
@@ -23,13 +23,61 @@ namespace WebFormEjemplo1.dal
 
 
         }
+        public static void Eliminar(int compraId)
+        {
+            var compra = new Compras();
+            using (var grupo = new Model1())
+            {
+                compra = grupo.Compras.FirstOrDefault(c => c.CompraId == compraId);
+                grupo.Compras.Remove(compra); // marco el objeto para eliminar
+                grupo.SaveChanges();
+            }
+
+        }
+        public static void Modificar(Compras compra)
+        {
+            using (var grupo = new Model1())
+            {
+                var compraAntigua = grupo.Compras
+                    .FirstOrDefault(c => c.CompraId == compra.CompraId); //Proxy
+                compraAntigua.CompraId = compra.CompraId;
+                compraAntigua.Cantidad = compra.Cantidad;
+                compraAntigua.PrecioUnitario = compra.PrecioUnitario;
+                compraAntigua.Producto = compra.Producto;
+
+                grupo.SaveChanges();
+            }
+
+        }
+        public static int Contar()
+        {
+            int total = 0;
+            using (var grupo = new Model1())
+            {
+                total = grupo.Compras.Count();
+            }
+            return total;
+        }
+
+
+        public static List<Compras> Listar2()
+        {
+            var resultado = new List<Compras>();
+            using (var grupo = new Model1())
+            {
+
+                resultado = grupo.Database.SqlQuery<Compras>("select * from compras").ToList();
+            }
+            return resultado;
+        }
 
         public static List<Compras> ListarTodo()
         {
             var resultado = new List<Compras>();
             using (var grupo = new Model1())
             {
-                resultado = grupo.Compras.ToList();
+                // resultado = grupo.Compras.ToList();
+                resultado = grupo.Compras.Include("Usuario").ToList();
             }
             return resultado;
         }
